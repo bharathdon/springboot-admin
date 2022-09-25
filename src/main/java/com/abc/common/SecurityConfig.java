1a073e5065c1697602d57040b684281957ac85c3
bharath <jrt1@outlook.in>
2022-09-25 16:18:11 +0530

    Initial commit

---
+++ src/main/java/com/abc/common/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.abc.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.csrf().disable();
+http.authorizeRequests().antMatchers("/**").permitAll().anyRequest().authenticated();
+		http.httpBasic();
+	}
+}
